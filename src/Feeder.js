06991863d59c14ffd62ceb1edfb5da2263bb79a8
seraco <sebastiaramon_1994@hotmail.com>
2017-05-07 01:07:19 +0200

    Afegir props als components

--- src/Feeder.js
+++ src/Feeder.js
@@ -11,8 +11,24 @@ class Feeder extends Component {
     return (
       <div className="feeder-grid">
         <FacebookView />
-        <TwitterView />
-        <MediumView />
+        <TwitterView
+          createdAt="Fri May 05 20:00:03 +0000 2017"
+          name="Node.js"
+          username="nodejs"
+          // verified={true}
+          text="✨ v7.10.0 of Node.js is out at https://t.co/q9wWuFYNdi with bug fixes, better WHATWG url compliance and crypto.randomFill{Sync}()"
+          // retweets={41}
+          // favourites={47}
+          profileImage="http://pbs.twimg.com/profile_images/702185727262482432/n1JRsFeB_normal.png"
+        />
+        <MediumView
+          title="Hello John"
+          subtitle="Pepjo is a fucking fucker"
+          // link=""
+          pubDate="2 May"
+          author="Dani Carmona"
+          // image=""
+        />
       </div>
     );
   }
--- src/js/TwitterView.js
+++ src/js/TwitterView.js
@@ -1,66 +1,63 @@
 import React, { Component } from 'react';
-
-import '../css/TwitterView_tCoreBundle.css';
-import '../css/TwitterView_tMore2Bundle.css';
-import '../css/TwitterView.css';
+import CSSModules from 'react-css-modules';
+import styles from '../css/TwitterView.css';
 
 class TwitterView extends Component {
   render() {
     return (
-      <div className="twitter-container">
-      <li className="js-stream-item stream-item stream-item
+      <div styleName="twitter-container">
+      <li styleName="stream-item stream-item
 " data-item-id="860595796252741632" id="stream-item-tweet-860595796252741632" data-item-type="tweet" data-suggestion-json="{&quot;suggestion_details&quot;:{},&quot;tweet_ids&quot;:&quot;860595796252741632&quot;,&quot;scribe_component&quot;:&quot;tweet&quot;}">
 
 
 
-  <div className="tweet js-stream-tweet js-actionable-tweet js-profile-popup-actionable dismissible-content
-       original-tweet js-original-tweet
+  <div styleName="tweet
 
-       has-cards  has-content
+       has-content
 " data-tweet-id="860595796252741632" data-item-id="860595796252741632" data-permalink-path="/NASAAero/status/860595796252741632" data-conversation-id="860595796252741632" data-tweet-nonce="860595796252741632-060edaad-780d-4c5d-9354-a84bdb84fcd9" data-tweet-stat-initialized="true" data-screen-name="NASAAero" data-name="NASA Aeronautics" data-user-id="2734381938" data-you-follow="false" data-follows-you="false" data-you-block="false" data-reply-to-users-json="[{&quot;id_str&quot;:&quot;2734381938&quot;,&quot;screen_name&quot;:&quot;NASAAero&quot;,&quot;name&quot;:&quot;NASA Aeronautics&quot;,&quot;emojified_name&quot;:{&quot;text&quot;:&quot;NASA Aeronautics&quot;,&quot;emojified_text_as_html&quot;:&quot;NASA Aeronautics&quot;}}]" data-disclosure-type="" data-has-cards="true">
 
-    <div className="context">
+    <div styleName="context">
 
 
     </div>
 
-    <div className="content">
+    <div styleName="content">
 
 
 
 
 
-      <div className="stream-item-header">
-          <a className="account-group js-account-group js-action-profile js-user-profile-link js-nav" href="/NASAAero" data-user-id="2734381938">
-    <img className="avatar js-action-profile-avatar" src="https://pbs.twimg.com/profile_images/500376882857005057/-3TY6rY6_bigger.jpeg" alt="" />
-    <span className="FullNameGroup">
-      <strong className="fullname show-popup-with-id " data-aria-label-part="">NASA Aeronautics</strong><span>‏</span><span className="UserBadges"><span className="Icon Icon--verified"><span className="u-hiddenVisually">Cuenta verificada</span></span></span><span className="UserNameBreak">&nbsp;</span></span><span className="username u-dir" dir="ltr" data-aria-label-part="">@<b>NASAAero</b></span></a>
+      <div styleName="stream-item-header">
+          <a styleName="account-group" href="/NASAAero" data-user-id="2734381938">
+    <img styleName="avatar" src={this.props.profileImage} alt="" />
+    <span>
+      <strong styleName="fullname" data-aria-label-part="">{this.props.name}</strong><span>‏</span><span styleName="UserBadges"><span styleName="Icon Icon--verified"><span styleName="u-hiddenVisually">Cuenta verificada</span></span></span><span>&nbsp;</span></span><span styleName="username u-dir" dir="ltr" data-aria-label-part="">@<b>{this.props.username}</b></span></a>
 
 
-        <small className="time">
-  <a href="/NASAAero/status/860595796252741632" className="tweet-timestamp js-permalink js-nav js-tooltip" title="13:43 - 5 may. 2017" data-conversation-id="860595796252741632"><span className="_timestamp js-short-timestamp js-relative-timestamp" data-time="1494017001" data-time-ms="1494017001000" data-long-form="true" aria-hidden="true">18 h</span><span className="u-hiddenVisually" data-aria-label-part="last">Hace 18 horas</span></a>
+        <small styleName="time">
+  <a href="/NASAAero/status/860595796252741632" styleName="tweet-timestamp" title="13:43 - 5 may. 2017" data-conversation-id="860595796252741632"><span data-time="1494017001" data-time-ms="1494017001000" data-long-form="true" aria-hidden="true">{this.props.createdAt}</span><span styleName="u-hiddenVisually" data-aria-label-part="last">Hace 18 horas</span></a>
 </small>
 
-          <div className="ProfileTweet-action ProfileTweet-action--more js-more-ProfileTweet-actions">
-    <div className="dropdown">
-  <button className="ProfileTweet-actionButton u-textUserColorHover dropdown-toggle js-dropdown-toggle" type="button">
-      <div className="IconContainer js-tooltip" title="Más">
-        <span className="Icon Icon--caretDownLight Icon--smallest"></span>
-        <span className="u-hiddenVisually">Más</span>
+          <div styleName="ProfileTweet-action ProfileTweet-action--more">
+    <div styleName="dropdown">
+  <button styleName="ProfileTweet-actionButton u-textUserColorHover dropdown-toggle" type="button">
+      <div styleName="IconContainer" title="Más">
+        <span styleName="Icon Icon--caretDownLight Icon--smallest"></span>
+        <span styleName="u-hiddenVisually">Más</span>
       </div>
   </button>
-  <div className="dropdown-menu is-autoCentered">
-  <div className="dropdown-caret">
-    <div className="caret-outer"></div>
-    <div className="caret-inner"></div>
+  <div styleName="dropdown-menu is-autoCentered">
+  <div styleName="dropdown-caret">
+    <div styleName="caret-outer"></div>
+    <div styleName="caret-inner"></div>
   </div>
   <ul>
 
-      <li className="copy-link-to-tweet js-actionCopyLinkToTweet">
-        <button type="button" className="dropdown-link">Copiar enlace del Tweet</button>
+      <li>
+        <button type="button" styleName="dropdown-link">Copiar enlace del Tweet</button>
       </li>
-      <li className="embed-link js-actionEmbedTweet" data-nav="embed_tweet">
-        <button type="button" className="dropdown-link">Insertar Tweet</button>
+      <li data-nav="embed_tweet">
+        <button type="button" styleName="dropdown-link">Insertar Tweet</button>
       </li>
   </ul>
 </div>
@@ -73,16 +70,16 @@ class TwitterView extends Component {
 
 
 
-        <div className="js-tweet-text-container">
-  <p className="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text" lang="en" data-aria-label-part="0">Reminder: the Competitive Program for Science Museums, Planetariums and NASA Visitors Centers deadline is June 19! <a href="https://t.co/Ukdu1ZzMrE" rel="nofollow noopener" dir="ltr" data-expanded-url="https://go.nasa.gov/2pigjd4" className="twitter-timeline-link" target="_blank" title="https://go.nasa.gov/2pigjd4"><span className="tco-ellipsis"></span><span className="invisible">https://</span><span className="js-display-url">go.nasa.gov/2pigjd4</span><span className="invisible"></span><span className="tco-ellipsis"><span className="invisible">&nbsp;</span></span></a><a href="https://t.co/zrB4oY96VZ" className="twitter-timeline-link u-hidden" data-pre-embedded="true" dir="ltr">pic.twitter.com/zrB4oY96VZ</a></p>
+        <div>
+  <p styleName="TweetTextSize TweetTextSize--normal" lang="en" data-aria-label-part="0">{this.props.text} <a href="https://t.co/Ukdu1ZzMrE" rel="nofollow noopener" dir="ltr" data-expanded-url="https://go.nasa.gov/2pigjd4" target="_blank" title="https://go.nasa.gov/2pigjd4"><span></span><span styleName="invisible">https://</span><span>go.nasa.gov/2pigjd4</span><span styleName="invisible"></span><span><span styleName="invisible">&nbsp;</span></span></a><a href="https://t.co/zrB4oY96VZ" styleName="u-hidden" data-pre-embedded="true" dir="ltr">pic.twitter.com/zrB4oY96VZ</a></p>
 </div>
 
 
 
 
 
-            <div className="AdaptiveMediaOuterContainer">
-    <div className="AdaptiveMedia
+            <div styleName="AdaptiveMediaOuterContainer">
+    <div styleName="AdaptiveMedia
 
 
         is-square
@@ -90,9 +87,9 @@ class TwitterView extends Component {
 
 
         ">
-      <div className="AdaptiveMedia-container">
-          <div className="AdaptiveMedia-singlePhoto">
-    <div className="AdaptiveMedia-photoContainer js-adaptive-photo " data-image-url="https://pbs.twimg.com/media/C_FzSfGXcAIbNQ9.jpg" data-element-context="platform_photo_card">
+      <div>
+          <div styleName="AdaptiveMedia-singlePhoto">
+    <div styleName="AdaptiveMedia-photoContainer" data-image-url="https://pbs.twimg.com/media/C_FzSfGXcAIbNQ9.jpg" data-element-context="platform_photo_card">
   <img data-aria-label-part="" src="https://pbs.twimg.com/media/C_FzSfGXcAIbNQ9.jpg" alt="" style={{width: '100%', top: '-0px'}} />
 </div>
 
@@ -111,89 +108,89 @@ class TwitterView extends Component {
 
 
 
-      <div className="stream-item-footer">
+      <div styleName="stream-item-footer">
 
-      <div className="ProfileTweet-actionCountList u-hiddenVisually">
+      <div styleName="u-hiddenVisually">
 
 
-    <span className="ProfileTweet-action--reply u-hiddenVisually">
-      <span className="ProfileTweet-actionCount" aria-hidden="true" data-tweet-stat-count="0">
-        <span className="ProfileTweet-actionCountForAria">0 respuestas</span>
+    <span styleName="ProfileTweet-action--reply u-hiddenVisually">
+      <span styleName="ProfileTweet-actionCount" aria-hidden="true" data-tweet-stat-count="0">
+        <span>0 respuestas</span>
       </span>
     </span>
-    <span className="ProfileTweet-action--retweet u-hiddenVisually">
-      <span className="ProfileTweet-actionCount" data-tweet-stat-count="3">
-        <span className="ProfileTweet-actionCountForAria" data-aria-label-part="">3 retweets</span>
+    <span styleName="ProfileTweet-action--retweet u-hiddenVisually">
+      <span styleName="ProfileTweet-actionCount" data-tweet-stat-count="3">
+        <span data-aria-label-part="">3 retweets</span>
       </span>
     </span>
-    <span className="ProfileTweet-action--favorite u-hiddenVisually">
-      <span className="ProfileTweet-actionCount" data-tweet-stat-count="5">
-        <span className="ProfileTweet-actionCountForAria" data-aria-label-part="">5 Me gusta</span>
+    <span styleName="ProfileTweet-action--favorite u-hiddenVisually">
+      <span styleName="ProfileTweet-actionCount" data-tweet-stat-count="5">
+        <span data-aria-label-part="">5 Me gusta</span>
       </span>
     </span>
   </div>
 
-  <div className="ProfileTweet-actionList js-actions" role="group" aria-label="Acciones del Tweet">
-    <div className="ProfileTweet-action ProfileTweet-action--reply">
-  <button className="ProfileTweet-actionButton js-actionButton js-actionReply" data-modal="ProfileTweet-reply" type="button">
-    <div className="IconContainer js-tooltip" title="Responder">
-      <span className="Icon Icon--reply"></span>
-      <span className="u-hiddenVisually">Responder</span>
+  <div styleName="ProfileTweet-actionList" role="group" aria-label="Acciones del Tweet">
+    <div styleName="ProfileTweet-action ProfileTweet-action--reply">
+  <button styleName="ProfileTweet-actionButton" data-modal="ProfileTweet-reply" type="button">
+    <div styleName="IconContainer" title="Responder">
+      <span styleName="Icon Icon--reply"></span>
+      <span styleName="u-hiddenVisually">Responder</span>
     </div>
-      <div className="IconTextContainer">
-        <span className="ProfileTweet-actionCount ProfileTweet-actionCount--isZero ">
-          <span className="ProfileTweet-actionCountForPresentation" aria-hidden="true"></span>
+      <div styleName="IconTextContainer">
+        <span styleName="ProfileTweet-actionCount ProfileTweet-actionCount--isZero ">
+          <span aria-hidden="true"></span>
         </span>
       </div>
   </button>
 </div>
-    <div className="ProfileTweet-action ProfileTweet-action--retweet js-toggleState js-toggleRt">
-  <button className="ProfileTweet-actionButton  js-actionButton js-actionRetweet" data-modal="ProfileTweet-retweet" type="button">
-    <div className="IconContainer js-tooltip" title="Retwittear">
-      <span className="Icon Icon--retweet"></span>
-      <span className="u-hiddenVisually">Retwittear</span>
+    <div styleName="ProfileTweet-action ProfileTweet-action--retweet">
+  <button styleName="ProfileTweet-actionButton" data-modal="ProfileTweet-retweet" type="button">
+    <div styleName="IconContainer" title="Retwittear">
+      <span styleName="Icon Icon--retweet"></span>
+      <span styleName="u-hiddenVisually">Retwittear</span>
     </div>
-      <div className="IconTextContainer">
-        <span className="ProfileTweet-actionCount">
-          <span className="ProfileTweet-actionCountForPresentation" aria-hidden="true">3</span>
+      <div styleName="IconTextContainer">
+        <span styleName="ProfileTweet-actionCount">
+          <span aria-hidden="true">3</span>
         </span>
       </div>
-  </button><button className="ProfileTweet-actionButtonUndo js-actionButton js-actionRetweet" data-modal="ProfileTweet-retweet" type="button">
-    <div className="IconContainer js-tooltip" title="Deshacer Retweet">
-      <span className="Icon Icon--retweet"></span>
-      <span className="u-hiddenVisually">Retwitteado</span>
+  </button><button styleName="ProfileTweet-actionButtonUndo" data-modal="ProfileTweet-retweet" type="button">
+    <div styleName="IconContainer" title="Deshacer Retweet">
+      <span styleName="Icon Icon--retweet"></span>
+      <span styleName="u-hiddenVisually">Retwitteado</span>
     </div>
-      <div className="IconTextContainer">
-        <span className="ProfileTweet-actionCount">
-          <span className="ProfileTweet-actionCountForPresentation" aria-hidden="true">3</span>
+      <div styleName="IconTextContainer">
+        <span styleName="ProfileTweet-actionCount">
+          <span aria-hidden="true">3</span>
         </span>
       </div>
   </button>
 </div>
 
-    <div className="ProfileTweet-action ProfileTweet-action--favorite js-toggleState">
-  <button className="ProfileTweet-actionButton js-actionButton js-actionFavorite" type="button">
-    <div className="IconContainer js-tooltip" title="Me gusta">
-      <div className="HeartAnimationContainer">
-        <div className="HeartAnimation"></div>
+    <div styleName="ProfileTweet-action ProfileTweet-action--favorite">
+  <button styleName="ProfileTweet-actionButton" type="button">
+    <div styleName="IconContainer" title="Me gusta">
+      <div styleName="HeartAnimationContainer">
+        <div styleName="HeartAnimation"></div>
       </div>
-      <span className="u-hiddenVisually">Me gusta</span>
+      <span styleName="u-hiddenVisually">Me gusta</span>
     </div>
-      <div className="IconTextContainer">
-        <span className="ProfileTweet-actionCount">
-          <span className="ProfileTweet-actionCountForPresentation" aria-hidden="true">5</span>
+      <div styleName="IconTextContainer">
+        <span styleName="ProfileTweet-actionCount">
+          <span aria-hidden="true">5</span>
         </span>
       </div>
-  </button><button className="ProfileTweet-actionButtonUndo u-linkClean js-actionButton js-actionFavorite" type="button">
-    <div className="IconContainer js-tooltip" title="Deshacer me gusta">
-      <div className="HeartAnimationContainer">
-        <div className="HeartAnimation"></div>
+  </button><button styleName="ProfileTweet-actionButtonUndo u-linkClean" type="button">
+    <div styleName="IconContainer" title="Deshacer me gusta">
+      <div styleName="HeartAnimationContainer">
+        <div styleName="HeartAnimation"></div>
       </div>
-      <span className="u-hiddenVisually">Gustado</span>
+      <span styleName="u-hiddenVisually">Gustado</span>
     </div>
-      <div className="IconTextContainer">
-        <span className="ProfileTweet-actionCount">
-          <span className="ProfileTweet-actionCountForPresentation" aria-hidden="true">5</span>
+      <div styleName="IconTextContainer">
+        <span styleName="ProfileTweet-actionCount">
+          <span aria-hidden="true">5</span>
         </span>
       </div>
   </button>
@@ -224,4 +221,4 @@ class TwitterView extends Component {
   }
 }
 
-export default TwitterView;
+export default CSSModules(TwitterView, styles, {allowMultiple:true});
