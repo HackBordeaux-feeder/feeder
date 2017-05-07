1d25237cf9f5365821c587fd6c67ba4a4624fc78
Dani carmona serrat <danielcarmona55@gmail.com>
2017-05-07 05:09:19 +0200

    Fix option setting not reseting after submit

--- src/js/settings/Settings.js
+++ src/js/settings/Settings.js
@@ -10,13 +10,14 @@ class Settings extends Component {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.handleServiceChange = this.handleServiceChange.bind(this);
     this.state = {
       service: 'Facebook',
       option: ''
     }
   }
 
-  handleOptionChange(event) {
+  handleServiceChange(event) {
     console.log(event.target.value)
     this.setState({service: event.target.value});
   }
@@ -40,13 +41,17 @@ class Settings extends Component {
     })
   }
 
+  handleOptionChange(event) {
+   this.setState({option: event.target.value});
+ }
+
   renderOptions () {
     const service = this.state.service
     if (service === 'Facebook') {
       return (
         <div>
           <label htmlFor="settings__option" styleName="settings__label">Url of the person you want to follow</label>
-          <input id="settings__option" ref={(ref) => this.username = ref} styleName="settings__input"/>
+          <input id="settings__option" value={this.state.option} styleName="settings__input" onChange={this.handleOptionChange}/>
           <input styleName="settings__submit" type="submit" value="Add Source" />
         </div>
       )
@@ -54,7 +59,7 @@ class Settings extends Component {
       return (
         <div>
           <label htmlFor="settings__option" styleName="settings__label">Twitter username</label>
-          <input id="settings__option" ref={(ref) => this.username = ref} styleName="settings__input"/>
+          <input id="settings__option" ref={(ref) => this.option = ref} styleName="settings__input"/>
           <input styleName="settings__submit" type="submit" value="Add Source" />
         </div>
       )
@@ -62,7 +67,7 @@ class Settings extends Component {
       return (
         <div>
           <label htmlFor="settings__option" styleName="settings__label">Medium username</label>
-          <input id="settings__option" ref={(ref) => this.username = ref} styleName="settings__input"/>
+          <input id="settings__option" ref={(ref) => this.option = ref} styleName="settings__input"/>
           <input styleName="settings__submit" type="submit" value="Add Source" />
         </div>
       )
@@ -75,7 +80,7 @@ class Settings extends Component {
         <h1 styleName="settings__title">Add new feeds!</h1>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor="settings__source" styleName="settings__label">Select the source of the feed</label>
-          <select styleName="source__select" id="settings__source" onChange={this.handleOptionChange}>
+          <select styleName="source__select" id="settings__source" onChange={this.handleServiceChange}>
             <option value="Facebook">Facebook</option>
             <option value="Twitter">Twitter</option>
             <option value="Medium">Medium</option>
