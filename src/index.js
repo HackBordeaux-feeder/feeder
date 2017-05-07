212d73195e7ef970f9ed6346e6cea6091ebd057c
Pep Rodeja Ferrer <pepjo26@gmail.com>
2017-05-07 08:46:39 +0200

    Add logs for server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+
+console.log('ENVS', process.env)
+
+console.log('ENVS API', process.env.API_URL)
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
