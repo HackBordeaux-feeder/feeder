f0e0eba6751f616e743c43856a5cef00bfa3a751
Pep Rodeja Ferrer <pepjo26@gmail.com>
2017-05-07 12:57:53 +0200

    Remove photo twitter

--- src/js/TwitterView.js
+++ src/js/TwitterView.js
@@ -89,8 +89,6 @@ class TwitterView extends Component {
         ">
       <div>
           <div styleName="AdaptiveMedia-singlePhoto">
-    <div styleName="AdaptiveMedia-photoContainer" data-image-url="https://pbs.twimg.com/media/C_FzSfGXcAIbNQ9.jpg" data-element-context="platform_photo_card">
-  <img data-aria-label-part="" src="https://pbs.twimg.com/media/C_FzSfGXcAIbNQ9.jpg" alt="" style={{width: '100%', top: '-0px'}} />
 </div>
 
 
