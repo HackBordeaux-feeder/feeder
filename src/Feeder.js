789eb9b549f3af951fa9183a1fc26a2ca2722cf6
seraco <sebastiaramon_1994@hotmail.com>
2017-05-07 05:55:45 +0200

    Ordenar mal ordenat

--- src/Feeder.js
+++ src/Feeder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 import FacebookView from './js/FacebookView.js';
 import TwitterView from './js/TwitterView.js';
@@ -7,6 +8,10 @@ import MediumView from './js/MediumView.js';
 
 import CSSModules from 'react-css-modules';
 import styles from './Feeder.css';
+
+function getRandomID () {
+  return `${performance.now()}-${parseInt(Math.random() * 10000000, 10)}`
+}
 
 class Feeder extends Component {
 
@@ -18,24 +23,21 @@ class Feeder extends Component {
       facebookFeed: [],
     }
 
-    console.log('constructor')
-
-    // axios.get(`${process.env.API_URL || 'http://localhost:5000'}/medium`)
-    //   .then((response) => {
-    //     this.setState({
-    //       mediumFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'medium' }) ))
-    //     })
-    //   })
-    //   .catch(() => {
-    //     this.setState({
-    //       error: 'Error getting medium'
-    //     })
-    //   })
-    axios.get(`${process.env.API_URL || 'http://localhost:5000'}/twitter`)
+    axios.get(`${process.env.API_URL || 'http://localhost:5000'}/medium`, { withCredentials: true })
       .then((response) => {
-        console.log(response.data)
         this.setState({
-          twitterFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'twitter' }) ))
+          mediumFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'medium', key: getRandomID(), date: moment(el.pubDate)}) ))
+        })
+      })
+      .catch(() => {
+        this.setState({
+          error: 'Error getting medium'
+        })
+      })
+    axios.get(`${process.env.API_URL || 'http://localhost:5000'}/twitter`, { withCredentials: true })
+      .then((response) => {
+        this.setState({
+          twitterFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'twitter', key: getRandomID(), date: moment(el.createdAt)}) ))
         })
       })
       .catch(() => {
@@ -43,10 +45,10 @@ class Feeder extends Component {
           error: 'Error getting twitter'
         })
       })
-    // axios.get(`${process.env.API_URL || 'http://localhost:5000'}/facebook`)
+    // axios.get(`${process.env.API_URL || 'http://localhost:5000'}/facebook`, { withCredentials: true })
     //   .then((response) => {
     //     this.setState({
-    //       facebookFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'facebook' }) ))
+    //       facebookFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'facebook', key: getRandomID(), date: }) ))
     //     })
     //   })
     //   .catch(() => {
@@ -66,26 +68,31 @@ class Feeder extends Component {
           ...this.state.twitterFeed,
           ...this.state.facebookFeed
         ]
-        // .sort()
+        .sort(function (a, b) {
+          return a.date.toISOString() - b.date.toISOString()
+        })
         .map((item) => {
           if (item.provider === 'facebook') {
             return (
-              <FacebookView />
+              <FacebookView key={item.key}/>
             )
           } else if (item.provider === 'medium') {
             return (
               <MediumView
-                title="Hello John"
-                subtitle="Pepjo is a fucking fucker"
-                // link=""
-                pubDate="2 May"
-                author="Dani Carmona"
-                // image=""
+                key={item.key}
+                title={item.title}
+                subtitle={item.subtitle}
+                link={item.link}
+                pubDate={item.pubDate}
+                author={item.author}
+                // image={item.image}
               />
             )
           } else if (item.provider === 'twitter') {
             return (
               <TwitterView
+                key={item.key}
+                profileLink={"https://twitter.com/" + item.username}
                 createdAt={item.createdAt}
                 name={item.name}
                 username={item.username}
