803fafcb145d001963fcdb22e9ca28e3b784d717
seraco <sebastiaramon_1994@hotmail.com>
2017-05-07 03:53:49 +0200

    Afegint feed components dinamics

--- src/Feeder.js
+++ src/Feeder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import FacebookView from './js/FacebookView.js';
 import TwitterView from './js/TwitterView.js';
@@ -7,28 +8,98 @@ import MediumView from './js/MediumView.js';
 import './Feeder.css';
 
 class Feeder extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mediumFeed: [],
+      twitterFeed: [],
+      facebookFeed: [],
+    }
+
+    console.log('constructor')
+
+    // axios.get(`${process.env.API_URL || 'http://localhost:5000'}/medium`)
+    //   .then((response) => {
+    //     this.setState({
+    //       mediumFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'medium' }) ))
+    //     })
+    //   })
+    //   .catch(() => {
+    //     this.setState({
+    //       error: 'Error getting medium'
+    //     })
+    //   })
+    axios.get(`${process.env.API_URL || 'http://localhost:5000'}/twitter`)
+      .then((response) => {
+        console.log(response.data)
+        this.setState({
+          twitterFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'twitter' }) ))
+        })
+      })
+      .catch(() => {
+        this.setState({
+          error: 'Error getting twitter'
+        })
+      })
+    // axios.get(`${process.env.API_URL || 'http://localhost:5000'}/facebook`)
+    //   .then((response) => {
+    //     this.setState({
+    //       facebookFeed: response.data.map((el) => ( Object.assign({}, el, { provider: 'facebook' }) ))
+    //     })
+    //   })
+    //   .catch(() => {
+    //     this.setState({
+    //       error: 'Error getting facebook'
+    //     })
+    //   })
+
+
+  }
+
   render() {
     return (
       <div className="feeder-grid">
-        <FacebookView />
-        <TwitterView
-          createdAt="Fri May 05 20:00:03 +0000 2017"
-          name="Node.js"
-          username="nodejs"
-          // verified={true}
-          text="✨ v7.10.0 of Node.js is out at https://t.co/q9wWuFYNdi with bug fixes, better WHATWG url compliance and crypto.randomFill{Sync}()"
-          // retweets={41}
-          // favourites={47}
-          profileImage="http://pbs.twimg.com/profile_images/702185727262482432/n1JRsFeB_normal.png"
-        />
-        <MediumView
-          title="Hello John"
-          subtitle="Pepjo is a fucking fucker"
-          // link=""
-          pubDate="2 May"
-          author="Dani Carmona"
-          // image=""
-        />
+        {[
+          ...this.state.mediumFeed,
+          ...this.state.twitterFeed,
+          ...this.state.facebookFeed
+        ]
+        // .sort()
+        .map((item) => {
+          if (item.provider === 'facebook') {
+            return (
+              <FacebookView />
+            )
+          } else if (item.provider === 'medium') {
+            return (
+              <MediumView
+                title="Hello John"
+                subtitle="Pepjo is a fucking fucker"
+                // link=""
+                pubDate="2 May"
+                author="Dani Carmona"
+                // image=""
+              />
+            )
+          } else if (item.provider === 'twitter') {
+            return (
+              <TwitterView
+                createdAt={item.createdAt}
+                name={item.name}
+                username={item.username}
+                // verified={item.verified}
+                text={item.text}
+                // retweets={item.retweets}
+                // favourites={item.favourites}
+                profileImage={item.profileImage}
+              />
+            )
+          } else {
+            console.log('null')
+            return null
+          }
+        })}
       </div>
     );
   }
